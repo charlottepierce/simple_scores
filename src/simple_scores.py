3bcb53ebc7fd9614db6eb63fe96c0b2991164c93
Charlotte Pierce <charlotte@malformed-bits.com>
2013-04-07 22:10:01 +1000

    Added script to run the main program.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

---
+++ src/simple_scores.py
@@ -0,0 +1,24 @@
+import sys
+
+import gui
+
+if __name__ == '__main__':
+	print ' -- Simple Scores -- '
+
+	if len(sys.argv) < 2:
+		print "No score given."
+		print
+		sys.exit(0)
+
+	print 'Using score ... ',
+	score_file = sys.argv[1]
+	print score_file
+
+	print 'Creating score viewer ... ',
+ 	score_viewer = gui.ScoreViewer(score_file)
+	print 'done.'
+
+	print 'Opening score viewer ... ',
+	score_viewer.view()
+	print 'done.'
+
