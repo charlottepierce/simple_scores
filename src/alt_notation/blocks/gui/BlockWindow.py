5588e11e0f00bcd8d6071082184ddd186306e5e4
Charlotte Pierce <charlotte@malformed-bits.com>
2013-04-15 19:55:57 +1000

    Added some documentation.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- src/alt_notation/blocks/gui/BlockWindow.py
+++ src/alt_notation/blocks/gui/BlockWindow.py
@@ -4,20 +4,45 @@ import gui
 
 class BlockWindow(pyglet.window.Window):
 	def __init__(self, width, height):
+		"""Create a BlockWindow object.
+
+		The window will update itself 60 times a second.
+
+		args
+		----
+			width:
+				The initial width of the window.
+
+			height:
+				The initial height of the window.
+		"""
+
 		pyglet.window.Window.__init__(self, width=width, height=height)
 
 		self.batch = pyglet.graphics.Batch() # batch renderer
 		pyglet.clock.schedule_interval(self.update, 1.0/60.0) # call update 60 times a second
 
-		self.note = gui.NoteBlock(self.batch, x=200, y=200, width=100, height=100)
+		self.note = gui.NoteBlock(self.batch, x=200, y=200, width=100, height=100, color=(255, 255, 255, 150))
 
 	def update(self, dt):
-		# dt = number of seconds that have passed since last update
+		"""Update the window.
+
+		args
+		----
+			dt:
+				The number of seconds that have passed since the last update.
+		"""
+
 		print 'Updating game window'
 		print 'dt:', dt
 		self.note.x += dt
 
 	def on_draw(self):
+		"""Window drawing.
+
+		Clear the window and draw everything assigned to the windows batch renderer.
+
+		"""
 		self.clear()
 		self.batch.draw()
 
--- src/alt_notation/blocks/gui/NoteBlock.py
+++ src/alt_notation/blocks/gui/NoteBlock.py
@@ -1,10 +1,35 @@
 import pyglet
 
 class NoteBlock(pyglet.sprite.Sprite):
-	def __init__(self, batch, x=100, y=100, width=100, height=100):
-		# build a solid white image for the block
-		pattern = pyglet.image.SolidColorImagePattern((255, 255, 255, 255))
+	def __init__(self, batch, x=100, y=100, width=100, height=100, color=(255, 255, 255, 255)):
+		"""Create a NoteBlock object.
+
+		args
+		----
+			batch:
+				The batch renderer the note block will be drawn by.
+
+			x:
+				The x-coordinate of the block.
+
+			y:
+				The y-coordinate of the block.
+
+			width:
+				The width of the block.
+
+			height:
+				The height of the block.
+
+			color:
+				The color of the block, as a RGBA tuple.
+
+		"""
+
+		# build an image for the block
+		pattern = pyglet.image.SolidColorImagePattern(color)
 		image = pyglet.image.create(width, height, pattern)
 
+		# create sprite, add to nominated batch renderer
 		pyglet.sprite.Sprite.__init__(self, image, x, y, batch=batch)
 
